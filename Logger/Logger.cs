75034d3c4deeb7791e81c7505d56ae05cccdabe5
sericaer <sericaer@outlook.com>
2021-08-04 14:44:16 +0800

    Add Logger

---
+++ Logger/Logger.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Tais
+{
+    public static class Logger
+    {
+        private static Action<string> log;
+
+        public static void Init(Action<string> logAction)
+        {
+            log = logAction;
+        }
+
+        public static void INFO(string str)
+        {
+            log?.Invoke(str);
+        }
+    }
+}
